fa871810336e11f6f86807d99288ff4c6db1345e
Alek <agrinb@gmail.com>
2015-03-04 00:13:33 -0500

    Adding comments

--- pyhash/hash_me.py
+++ pyhash/hash_me.py
@@ -1,27 +1,22 @@
 import pdb
+import uuid
 
 class Hash_me(object):
     def __init__(self, *args):
+        if len(args) % 2 != 0:
+            raise  RuntimeError ("Number of arguments must be even")
+        if len(args) < 2:
+            raise  RuntimeError("Hash takes at least two arguments")  
         self.bin_count = 10
         self.bins = [None] * 10
-        if len(args) < 2:
-            key = args[0]
-            value = args[1]
-            index = self.init_idx(key)
-            try:
-                self.bins[index] = [key, value]
-            except IndexError:
-                self.bins.insert(index, [key, value])
-        else:
-            for idx, val in enumerate(args):
-                arr = args
-                if idx < len(arr) - 1 and (idx == 0 or idx % 2 == 0):
-                    # pdb.set_trace()
-                    index = self.init_idx(arr[idx])
-                    try:
-                        self.bins[index] = [arr[idx], arr[idx+1]]
-                    except IndexError:
-                        self.bins.insert(index, [arr[idx], arr[idx+1]])
+        for idx, val in enumerate(args):
+            arr = args
+            if idx < len(arr) - 1 and (idx == 0 or idx % 2 == 0):
+                index = self.init_idx(arr[idx])
+                try:
+                    self.bins[index] = [arr[idx], arr[idx+1]]
+                except IndexError:
+                    self.bins.insert(index, [arr[idx], arr[idx+1]])
 
 
     def __iter__(self):
@@ -55,7 +50,6 @@ class Hash_me(object):
 
     def get(self, key):
         index = self.get_idx(key)
-        # pdb.set_trace()
         return self.bins[index][1]
     
 
@@ -76,7 +70,7 @@ class Hash_me(object):
 
     def merge(self, entry):
         for index, item in enumerate(entry):
-            if item != None:
+            if item != None: 
                 if self.bins[index] == None:
                     self.bins[index] = item
                     return self
@@ -85,30 +79,29 @@ class Hash_me(object):
                     return self
 
     def init_idx(self, key):
-        index = self.bin_for(key)
         bin_len = len(self.bins)
+        # if you are running out of slots in the array double it
         if self.bins.count(None) < bin_len / 2:
             self.double_bins
+        index = self.bin_for(key)
         while True:
-            if self.bins[index] == None:
+            if self.bins[index] == None: # Get an empty slot at said index, else, see if the next slot is empty
                 return index
-            else: 
-                index += 1
+            else:  
+                index += 1 
 
+    # Check if key is at said index, otherwise check next slot
     def get_idx(self, key):
         index = self.bin_for(key)
-        switch = True
-        while switch:
+        while True: 
             if self.bins[index][0] == key:
                 return index
             else: 
                 index += 1
 
-
-
-
+    #calculate the index to store and retrieve the pair of values
     def bin_for(self, key):
-        return id(key) % self.bin_count 
+        return hash(key) % self.bin_count 
 
     bin_count = property(get_bin_count, set_bin_count)
     bins = property(get_bins, set_bins)
--- pyhash/hash_test.py
+++ pyhash/hash_test.py
@@ -4,27 +4,34 @@ import unittest
 import pdb
 
 
-
-
 class HashMeTest(unittest.TestCase):
 
-
+    #I focussed on the functionality of the Class rather than
+    #syntax, I did not create a hash literal method.
+    
+    #ha is an instance of Hash_me
     def test_init(self):
         ha = Hash_me(5,6)
+        ha2 = Hash_me('string', 1)
+        ha3 = Hash_me('string1', "string2")
         self.assertIsInstance(ha, Hash_me)
+        self.assertIsInstance(ha2, Hash_me)
+        self.assertIsInstance(ha3, Hash_me)
 
-    def test_add(self):
-        ha = Hash_me(5,6,7,8,1,2,3,4)
+    #instantiating a Hash with more that two 
+    def test_init_mult(self):
         ha2 = Hash_me(5,6,7,8,1,2,3,4,11,12,13,14,15,16,17,18)
-        self.assertEquals(ha.get(5), 6)
-        self.assertEquals(ha2.get(7), 8)
-        self.assertEquals(ha2.get(1), 2)
-        self.assertEquals(ha2.get(3), 4)
+        self.assertEquals(ha2.get(5), 6)
         self.assertEquals(ha2.get(11), 12)
-        self.assertEquals(ha2.get(13), 14)
-        self.assertEquals(ha2.get(15), 16)
         self.assertEquals(ha2.get(17), 18)
-      
+
+
+    def test_init_with_strings(self):
+        string1 = 'string1'
+        string2 = 'string2'
+        ha = Hash_me('string1', 'string2')
+        self.assertEquals(ha.get('string1'), 'string2')
+    
     def test_get(self):
         ha = Hash_me(8,9)
         self.assertEquals(ha.get(8), 9)
@@ -47,7 +54,6 @@ class HashMeTest(unittest.TestCase):
         ha.merge(ha2)
         self.assertItemsEqual(ha.keys(),[55, 8])
         self.assertItemsEqual(ha.values(),[66, 9])
-
 
 
 
