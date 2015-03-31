e1d5bddd3ca5288e55ec13a61c1c653aae44772e
Alek <agrinb@gmail.com>
2015-03-30 23:01:02 -0400

    All test passing

--- ticker.py
+++ ticker.py
@@ -9,46 +9,40 @@ import csv
 import sys
 
 
-
-
-
 class Ticker(object):
     def __init__(self):
         self.ticker = ''
         self.cik_url_prop = "http://www.sec.gov/cgi-bin/browse-edgar?CIK={}&Find=Search&owner=exclude&action=getcompany"
         self.f13_url_prop = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type=13F-HR&dateb=&count=40&scd=filings"
         self.sec_url = 'http://www.sec.gov/'
-        self.company = 'Temp'
         self.cik = None
-
-
-
-    # def get_ticker(self):
-    #     return self.__ticker
-
-    # def set_ticker(self, value):
-    #     self.__ticker = value
 
     def init_input(self):
         print "Please input a ticker or CIK#:"
         inp = raw_input()
+        return inp
+
+    #test whether interger(CIK) or ticker
+    def set_input(self, inp):
         try: 
-            if isinstance(((int(inp[0]))), int) and len(inp) == 10:
+            #testing first chr because CIK start with 000
+            if isinstance(((int(inp[0]))), int) and len(inp) == 10: 
                 self.cik = inp
-                return self.get_report_dom(self.cik_url_prop)
+                return self.cik
         except ValueError:
             print "{} is a Ticker".format(inp) 
-        else:
-            print '2'
-            self.set_ticker_attr(inp)
-            self.a_nodes_to_cik(self.cik_url_prop)
-    
+            return self.get_cik_num(inp)
+        
+    def get_cik_num(self, inp): 
+        self.set_ticker_attr(inp)
+        self.page_with_cik(self.cik_url_prop)
+           
     def set_ticker_attr(self, inp):
         while len(inp) >= 6 or len(inp) <= 3 :
             print "Sorry it does not appear you entered a valid ticker, please try again:"
             inp = raw_input()
         self.ticker = inp
-
+        return self.ticker
 
     def get_dom(self, link):
         page = urllib2.urlopen(link.format(self.ticker)).read()
@@ -58,10 +52,9 @@ class Ticker(object):
     def get_cik(self, link):
       num = re.findall(r'(?<=action=getcompany&CIK=).*?(?=&)', link)
       self.cik = num[0]
+      return self.cik 
   
-
     def find_in_string(self, link):
-        word = '?action=getcompany&CIK='
         match = re.search(r'action=getcompany&CIK=', link)
         try: 
            if match.group(0) is not None:
@@ -70,38 +63,33 @@ class Ticker(object):
         else: 
             return False
 
-    def a_nodes_to_cik(self, link):
+    def page_with_cik(self, link):
         dom = self.get_dom(link)
         a_nodes = dom.find_all('a')
         for a in a_nodes:
             href = a.get('href')
             if self.find_in_string(href):
-                self.get_cik(href)
-                self.get_report_dom(self.cik_url_prop)
+                return self.get_cik(href)
 
-
-    def get_report_dom(self, link):
-        # #####
-        # temp = "http://www.sec.gov/cgi-bin/browse-edgar?CIK=0001166559&Find=Search&owner=exclude&action=getcompany"
-        # #####
-        # cik = self.a_nodes_to_cik(self.cik_url_prop)
-      
+    def get_report_dom(self):
         f13_link = self.f13_url_prop.format(self.cik)
         reports_dom = self.get_dom(f13_link)
-        self.get_report_path(reports_dom)
+        return reports_dom
 
     def get_report_path(self, dom):
-        # pdb.set_trace()
-        cells = dom.select(".tableFile2")[0].find_all('td')
-        the_cell = None
-        for cell in reversed(cells):
-            if cell.string == '13F-HR':
-                the_cell = cell
-        href = the_cell.find_next().find('a')['href']
-        report_url = self.sec_url + href
-        self.get_docs_path(report_url)
-
-
+        if not dom.select(".tableFile2")[0].find_all('td'):
+            print "{} does not have a 13F-HR report asscociated with it".format(self.ticker)
+            raise SystemExit
+        else: 
+            cells = dom.select(".tableFile2")[0].find_all('td')
+            the_cell = None
+            for cell in reversed(cells):
+                if cell.string == '13F-HR':
+                    the_cell = cell
+            href = the_cell.find_next().find('a')['href']
+            report_url = self.sec_url + href
+            return report_url
+    
     def get_docs_path(self, link):
         dom = self.get_dom(link)
         cells = dom.select(".tableFile")[0].find_all('td')
@@ -113,16 +101,16 @@ class Ticker(object):
             except: TypeError
         href = the_cell.find_next().find('a')['href']
         file_url = self.sec_url + href
-        self.get_file_text(file_url)
-
+        return file_url
+     
     def get_file_text(self, link):
         page = urllib2.urlopen(link).read().replace('\n', '')
         data = re.findall ('<TEXT>(.*?)</TEXT>', page, re.DOTALL)
         data_xml = BeautifulSoup(data[1], "lxml")
-        self.to_csv(data_xml)
+        return data_xml
 
     def to_csv(self, data):
-        with open('{}.csv'.format(self.company), 'w') as csvfile:
+        with open('report.csv', 'w') as csvfile:
             co = data.find('infotable')
             fieldnames = []
             for child in co.children:
@@ -137,37 +125,24 @@ class Ticker(object):
                     if hasattr(child, 'tag'):
                          string = unicode(child.string)
                          row['{}'.format(child.name)] = string 
-                writer.writerow(row) 
-                print '1'       
+                writer.writerow(row)       
             csvfile.close()
-
-
+        return csvfile
     
     def read_text_file(self, link):  
         page = urllib2.urlopen(link)
         report = list(csv.reader(page , delimiter='\t'))
         for line in report:
             print line
-        # with open(page) as tsv:
 
-          
-
-
-
-
-    #get a elements
-    #find a link that includes 'action=getcompany&CIK'
-    #parse out the number between 'action=getcompany&CIK' and '&owner'
-    #check that parsed info is a number or return warning
-    #request html at http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001177017&type=13F-HR&dateb=&count=40&scd=filings using the CIK 
-    #look in the table element, find row with a cell that includes '13F-HR'
-    #in the same row find cell with "documentsbutton" id
-    #get url for 'documentsbody' id 
-    #find row elem with string "Complete submission text file"
-    #find link in the same row
-    #parse all rows for "<nameOfIssuer>"
-    #in the same element get <value>
-
+    def ticker_flow(self): 
+        inp = self.init_input()
+        self.set_input(inp)
+        report_dom = self.get_report_dom()
+        report_url = self.get_report_path(report_dom)
+        file_url = self.get_docs_path(report_url)
+        data_xml = self.get_file_text(file_url)
+        return self.to_csv(data_xml)
 
 
 
--- ticker_test.py
+++ ticker_test.py
@@ -1,6 +1,7 @@
 from ticker import Ticker
 import unittest
 import pdb 
+import csv
 
 
 
@@ -10,42 +11,79 @@ class TickerTest(unittest.TestCase):
         self.tk = Ticker()
         self.tk.cik_url_prop = 'http://www.sec.gov/cgi-bin/browse-edgar?CIK={}&Find=Search&owner=exclude&action=getcompany'
         self.tk.f13_url_prop = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type=13F-HR&dateb=&count=40&scd=filings'
-
-            
-    # def test_init(self):  
-    #     tk = Ticker()
-    #     self.assertIsInstance(tk, Ticker)
-
-    def test_setter(self):
+        
+    def test_init(self):  
+        tk = Ticker()
+        self.assertIsInstance(tk, Ticker)
+    # given a CIK, sets self.cik
+    def test_set_input(self):
+        inp = '0001166559'
+        self.tk.set_input(inp)
+        self.assertEquals(self.tk.cik, '0001166559')
+    #given a ticker, CIK is not set
+    def test_set_input_w_ticker(self):
         inp = 'PRGFX'
-        self.tk.ticker = inp
-        self.assertEquals(self.tk.init_input(), 'PRGFX')
+        self.tk.set_input(inp)
+        self.assertEquals(self.tk.cik, '0000080257')
+    #given a ticker, ticker is set
+    def test_set_ticker_attr(self):
+        inp = 'PRGFX'
+        self.tk.set_ticker_attr(inp)
+        self.assertEquals(self.tk.ticker, 'PRGFX')
+    #given a link, CIK is extracted and set
+    def test_get_cik(self):
+        link =  'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001177017&owner=exclude&count=40'
+        self.tk.get_cik(link)
+        self.assertEquals(self.tk.cik, '0001177017')
 
-    # def test_check_input(self):
-    #     inp = 'PRGFX'
-    #     self.assertEquals(self.tk.set_ticker_attr(inp), 'PRGFX')
+    def test_find_in_string(self):
+        link =  'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001177017&owner=exclude&count=40'
+        self.tk.find_in_string(link)
+        self.assertEquals(self.tk.find_in_string(link), True)
 
-    # def test_get_dom(self):
-    #     self.assertEquals(self.tk.get_dom(), 'PRGFX')
+    def test_page_with_cik(self):
+        link =  'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001177017&owner=exclude&count=40'
+        self.assertEquals(self.tk.page_with_cik(link), '0001177017')
+    #turn BeautifulSoup into a string and test for HTML 
+    def test_get_dom(self):
+        link =  'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001177017&owner=exclude&count=40'
+        self.assertEquals(unicode(self.tk.get_dom(link))[:9], '<!DOCTYPE')
 
-    # def test_matching(self):
-    #     self.assertEquals(self.tk.match(), 000116655 )
+    def test_get_report_dom(self):
+       self.assertEquals(unicode(self.tk.get_report_dom())[:9], '<!DOCTYPE')
 
-    # def test_find_in_string(self):
-    #     self.assertEquals(self.tk.find_in_string(), True )
+    def test_get_report_path(self):
+        link =  'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001166559&owner=exclude&count=40'
+        self.tk.get_cik(link)
+        dom = self.tk.get_dom(link)
+        self.tk.cik = '0001166559'
+        rdom = self.tk.get_report_dom()
+        self.assertEquals(self.tk.get_report_path(rdom), 'http://www.sec.gov//Archives/edgar/data/1166559/000110465915010462/0001104659-15-010462-index.htm')
 
-    # def cik_url_prop_test(self):
-    #     self.tk.ticker = 'PRGFX'
-    #     self.assertEquals(self.tk.a_nodes_to_cik(self.tk.cik_url_prop)[0], '0000080257' )
+    def test_get_docs_path(self):
+        link = 'http://www.sec.gov//Archives/edgar/data/1166559/000110465915010462/0001104659-15-010462-index.htm'
+        self.assertEquals(self.tk.get_docs_path(link), 'http://www.sec.gov//Archives/edgar/data/1166559/000110465915010462/0001104659-15-010462.txt')
 
-    # def test_report_dom(self):
-    #     self.tk.cik = '0001166559'
-    #     # self.tk.set_ticker_attr(self.tk.ticker)
-    #     #self.assertEquals(self.tk.get_report_dom(self.tk.f13_url_prop))
-    #     link = "http://www.sec.gov/cgi-bin/browse-edgar?CIK=0001166559&Find=Search&owner=exclude&action=getcompany"
-    #     self.assertEquals(self.tk.get_report_dom(link))
+    def get_file_text(self):
+        link = 'http://www.sec.gov//Archives/edgar/data/1166559/000110465915010462/0001104659-15-010462.txt'
+        self.assertEquals(unicode(self.tk.get_file_text(link))[:5], '<XML>')
 
-
+    def test_to_csv(self):
+        link = 'http://www.sec.gov//Archives/edgar/data/1166559/000110465915010462/0001104659-15-010462.txt'
+        data = self.tk.get_file_text(link)
+        csv_file = self.tk.to_csv(data) 
+        is_csv = None
+        with open('report.csv', 'rb') as csv_file:
+            try:
+                dialect = csv.Sniffer().sniff(csv_file.read(1024))
+                csv_file.seek(0)
+                is_csv = True
+            except: csv.Error
+            reader = csv.reader(csv_file, delimiter=',')
+            headers = reader.next()
+        self.assertEquals(is_csv, True)
+        self.assertEquals(headers[0], 'nameofissuer')
+        csv_file.close()
 
 
 
