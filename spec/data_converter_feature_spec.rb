84ee70b79ba0aa9910ce9b313bd6afca1887affd
Alek <agrinb@gmail.com>
2015-04-06 19:49:48 -0400

    feature tests passing

--- spec/data_converter_feature_spec.rb
+++ spec/data_converter_feature_spec.rb
@@ -1,25 +1,26 @@
 require 'pry'
 require './data_converter'
+require './server'
 require 'capybara/rspec'
 require 'spec_helper'
 require 'feature_spec_helper'
 
+feature "my test" do
+    scenario "visit site", :type => :feature do
+      visit '/gender_and_lastname'
+      expect(first(".list-group-item").text).to have_content("Hingis, Martina, M, F, 04/02/1979, Green")
+    end
 
-describe "the signin process", :type => :feature do
-  it "signs me in" do
-    visit '/gender_and_lastname'
-    expect(page).to have_content 'Abercrombie'
-  end
 
-  # it "signs me in" do
-  #   visit '/birth_date'
-  #   expect(page).to have_content 'Abercrombie'
-  # end
+   scenario "visit site", :type => :feature do
+      visit '/birth_date'
+      expect(first(".list-group-item").text).to have_content("Abercrombie, Neil, NA, M, 02/13/1943, Tan")
+    end
 
-  # it "signs me in" do
-  #   visit '/last_name'
-  #   expect(page).to have_content 'Abercrombie'
-  # end
+    scenario "visit site", :type => :feature do
+      visit '/last_name'
+      expect(first(".list-group-item").text).to have_content("Smith, Steve, D, M, 03/03/1985, Red")
+    end
 end
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,18 +3,20 @@ require 'rspec'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-@test_url = "test"
+# @test_url = "test"
 
-RSpec.configure do |config|
-  config.include Capybara::DSL
-end
+# RSpec.configure do |config|
+#   config.include Capybara::DSL
+# end
 
 Capybara.configure do |config|
   config.run_server = false
   config.current_driver = :selenium
-  config.app = "DataConverter"
+  config.app = Sinatra::Application
   config.app_host = "http://localhost:4567"
 
   # config.include Capybara::DSL, feature: true
   # config.include Capybara::RSpecMatchers, feature: true
 end
+
+
